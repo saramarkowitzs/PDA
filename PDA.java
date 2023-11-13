2ed269ed1791fb7c23de6e67a505f4511531d64a
Sara Markowitz <sara.markowitz@mrjaffesclass.com>
2023-11-13 11:20:31 -0800

    I nEED HELPPPP

---
+++ PDA.java
@@ -0,0 +1,39 @@
+/**
+ * Permissible Dating Age program
+ *
+ * @author Mr. Jaffe
+ * @version 2021-06-22 Version 1.0.0
+ */
+public class PDA
+{
+
+    /**
+     * Constructor for objects of class PDA
+     */
+    public PDA()
+    {
+        // We don't need to do anything in the constructor for
+        // our program.
+    }
+    
+    /**
+     * This is the main event loop for our PDA program
+     */
+    public void runEventLoop() {
+        scanner = new Scanner(System.in);
+       while (true) {
+      System.out.println("How old are you?");
+      age = scanner.nextInt();
+      System.out.println(age);
+    }
+    }
+
+    /**
+     * The main method instantiates and runs the program
+     */
+    public static void main (String[] args) {
+        PDA pda = new PDA();
+        pda.runEventLoop();
+    }
+}
+
